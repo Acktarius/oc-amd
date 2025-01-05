3bfd6a44e0494a1d9d2dbfef7142eae0a3da684c
acktarius <xchaps@aol.com>
2025-01-05 13:47:06 -0500

    function

--- functions.sh
+++ functions.sh
@@ -21,6 +21,6 @@ get_supported_devices() {
         to_entries[] | 
         select(.value.revision | to_entries[] | 
             .value | match($pattern)
-        ) | .key
+        ) | "\"" + .key + "\""
     ' "$DEVICES_JSON" | tr '\n' '|' | sed 's/|$//')
 } 
