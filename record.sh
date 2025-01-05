103daafbf2f27dfe0581eeba2e0050b99bd4feeb
acktarius <xchaps@aol.com>
2025-01-05 17:22:21 -0500

    legend

--- record.sh
+++ record.sh
@@ -152,7 +152,9 @@ for ((i = cardInit; i < 10; i++)); do
         done
 
         # Add plot commands with appropriate axes
-        echo "'/tmp/card${i}_data.txt' using 1:2:(\$3*100.0/255.0):(\$4/1000000.0) title 'Card${i} ${card_names[$i]} (GPU% / Fan% / Power W)' with lines lw 2 lc rgb '${color}', \\" >> /tmp/plot.gnu
+        echo "'/tmp/card${i}_data.txt' using 1:2 title 'Card${i} ${card_names[$i]}' with lines lw 2 lc rgb '${color}', \\" >> /tmp/plot.gnu
+        echo "'' using 1:(\$3*100.0/255.0) notitle with lines lw 2 lc rgb '${color}' dt 2, \\" >> /tmp/plot.gnu
+        echo "'' using 1:(\$4/1000000.0) notitle with lines lw 2 lc rgb '${color}' dt 3 axes x1y2, \\" >> /tmp/plot.gnu
     fi
 done
 
